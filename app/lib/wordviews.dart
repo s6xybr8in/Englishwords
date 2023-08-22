17602c9f6e7ef42e150c8bf5b5d0a1d5881c7d78
s6xybr8in <s6xybr8in@gmail.com>
2023-08-23 04:50:05 +0900

    Modeling 2.7

--- app/lib/wordviews.dart
+++ app/lib/wordviews.dart
@@ -10,9 +10,10 @@ class UNI with ChangeNotifier{
   UNI(this.chapter);
 }
 
+
+
 class WordViews extends StatefulWidget {
   WordViews({required this.chapter, super.key});
-
   final int chapter;
 
   @override
@@ -20,21 +21,23 @@ class WordViews extends StatefulWidget {
 }
 
 class _WordViewsState extends State<WordViews> {
+  _WordViewsState(this.chapter);
+
+
   List<Widget> bodylist = [];
-  var count = 0;
-  int selected = 0;
   List<bool> _selection = [true, false];
   final int chapter;
+  var count = 0;
+  int selected = 0;
 
-  _WordViewsState(this.chapter);
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    count = (COUNT - chapter * 40 > 39) ? 40 : COUNT - chapter * 40;
+    count = (COUNT - chapter * 40 > 39) ? 40 : COUNT - chapter * 40; //앞으로의 단어 갯수가 40이상 크면 MAX_COUNT 인 40으로 지정 아니면 남은 갯수만큼 지정
     bodylist.add(listview(count));
-    bodylist.add(Consumer(builder: (context, value, child) => DirectView(count: count),));
+    bodylist.add(DirectView(count: count));
   }
 
   @override
@@ -95,39 +98,41 @@ class _WordViewsState extends State<WordViews> {
 }
 
 
+
+//==================================================================================
+//==================================================================================
+//==================================================================================
+//==================================================================================
+////===============================Direct View======================================
 class DirectView extends StatefulWidget {
   final int count;
 
-  DirectView(
-      {required this.count,
-        super.key});
+  DirectView({required this.count});
 
   @override
-  State<DirectView> createState() =>
-      _DirectViewState(count: count);
+  State<DirectView> createState() => _DirectViewState(count: count);
 }
 
 class _DirectViewState extends State<DirectView> {
-  final int count;
+  final int count; //max count
+  List<bool> checked = List<bool>.filled(COUNT, false); //영여인지 뜻인지 check 전체 단어 갯수만큼
+  _DirectViewState({required this.count});
 
-  _DirectViewState(
-      {required this.count});
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
+  // setState() or markNeedsBuild() called during build. 오류 발생
+  // Bottom 의 up,down 버튼 누를때
+  // DirectView의 TextButton 눌렀을 때
+  // DirectView의 스크롤 고정됨
   @override
   Widget build(BuildContext context) {
+    final int chapter = Provider.of<UNI>(context,listen: false).chapter;
     return PageView.builder(
       controller: PageController(),
       scrollDirection: Axis.vertical,
       itemCount: count,
       itemBuilder: (context, index) {
         if (Provider.of<UNI>(context).INDEX > index){
-          index = Provider.of<UNI>(context).INDEX;
+          index = Provider.of<UNI>(context,listen: false).INDEX;
           Provider.of<UNI>(context,listen: false).notifyListeners();
         }
         else
@@ -137,17 +142,32 @@ class _DirectViewState extends State<DirectView> {
               child: Center(
                 child: TextButton(
                   onPressed: () {
-                      print(Provider.of<UNI>(context).INDEX);
-                      Provider.of<UNI>(context,listen: false).notifyListeners();
+                    checked[index+ chapter*40] ^= true;
+                    setState(() {});
                   },
-                  child: Text("${Keys[index + (Provider.of<UNI>(context).chapter * 40)]}",
-                      textScaleFactor: 3),),
+                  child: (checked[index+ chapter*40]==true) ? Text("${word[Keys[index + (chapter * 40)]]}",textScaleFactor: 2.4,) : Text("${Keys[index + (chapter * 40)]}",textScaleFactor: 3),),
               ),
             ));
       },
     );
   }
 }
+
+//==================================================================================
+//==================================================================================
+//==================================================================================
+//==================================================================================
+
+
+
+
+
+
+//==================================================================================
+//==================================================================================
+//==================================================================================
+//==================================================================================
+//=================================List View========================================
 
 class listview extends StatefulWidget {
   const listview(this.count, {super.key});
@@ -161,7 +181,6 @@ class listview extends StatefulWidget {
 class _listviewState extends State<listview> {
   List<bool> checked = [];
   final int count;
-
   _listviewState(this.count);
 
   @override
@@ -174,6 +193,7 @@ class _listviewState extends State<listview> {
 
   @override
   Widget build(BuildContext context) {
+    final int chapter = Provider.of<UNI>(context,listen: false).chapter;
     return ListView.builder(
         itemCount: count * 2,
         itemBuilder: (context, index) {
@@ -181,22 +201,22 @@ class _listviewState extends State<listview> {
           var Chapter = (realIndex) ~/ 50;
           if (index.isOdd) return Divider();
           return ListTile(
-            title: checked[Provider.of<UNI>(context,listen: false).chapter * 40 + realIndex] == true
+            title: checked[chapter * 40 + realIndex] == true
                 ? TEXT1(
-                word[Keys[Provider.of<UNI>(context,listen: false).chapter * 40 + realIndex]]!, FontWeight.bold, 20)
-                : TEXT1(Keys[Provider.of<UNI>(context,listen: false).chapter * 40 + realIndex], FontWeight.bold, 20),
+                word[Keys[chapter * 40 + realIndex]]!, FontWeight.bold, 20)
+                : TEXT1(Keys[chapter * 40 + realIndex], FontWeight.bold, 20),
             subtitle: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TEXT1("${Chapter + 1},${Provider.of<UNI>(context,listen: false).chapter * 40 + realIndex + 1}",
+                TEXT1("${Chapter + 1},${chapter * 40 + realIndex + 1}",
                     FontWeight.w400, 15),
                 IconButton(
                     onPressed: () {
                       setState(() {
-                        Scrap[Provider.of<UNI>(context,listen: false).chapter * 40 + realIndex] ^= 1;
+                        Scrap[chapter * 40 + realIndex] ^= 1;
                       });
                     },
-                    icon: (Scrap[Provider.of<UNI>(context,listen: false).chapter * 40 + realIndex] == 1)
+                    icon: (Scrap[chapter * 40 + realIndex] == 1)
                         ? Icon(
                       Icons.star,
                     )
@@ -206,9 +226,14 @@ class _listviewState extends State<listview> {
             ),
             onTap: () =>
                 setState(() {
-                  checked[Provider.of<UNI>(context,listen: false).chapter * 40 + realIndex] ^= true;
+                  checked[chapter * 40 + realIndex] ^= true;
                 }),
           );
         });
   }
 }
+
+//==================================================================================
+//==================================================================================
+//==================================================================================
+//==================================================================================
